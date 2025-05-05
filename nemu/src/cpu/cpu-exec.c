20e0df38935393c096c6c283db6134251b19deda
tracer-ysyx <tracer@ysyx.org>
2025-05-05 16:09:49 +0800

    > compile NEMU ysyx_22040000 迟浩岐 Linux c 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:49 up  5:19,  2 users,  load average: 0.03, 0.11, 0.21

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -32,12 +32,20 @@ static bool g_print_step = false;
 
 void device_update();
 
+int check_wps();  // 监视点的值发生变化时返回1,否则返回0；
+
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+
+#ifdef CONFIG_WATCHPOINT
+  if (check_wps()) {
+    nemu_state.state = NEMU_STOP;
+  }
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
