c79dbd1a23b275ac330f666991bdd73861e39300
tracer-ysyx <tracer@ysyx.org>
2025-05-10 19:02:53 +0800

    > compile NEMU ysyx_22040000 迟浩岐 Linux c 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:02:53 up 1 day, 20:13,  1 user,  load average: 0.33, 0.27, 0.22

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -45,8 +45,9 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   switch (type) {
     case TYPE_R: src1R(); src2R();
     case TYPE_I: src1R();          immI(); break;
-    case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_U:                   immU(); break;
     case TYPE_J:                   immJ(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
