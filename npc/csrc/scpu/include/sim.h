9a18e8ae69579a88e569cd5cb3fbe0090fe11d50
tracer-ysyx <tracer@ysyx.org>
2025-07-03 20:09:24 +0800

    > sim RTL ysyx_22040000 迟浩岐 Linux c 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:09:24 up 3 days, 11:13,  1 user,  load average: 0.35, 0.47, 0.50

--- npc/csrc/scpu/include/sim.h
+++ npc/csrc/scpu/include/sim.h
@@ -7,6 +7,12 @@ enum {
   NPC_STOP,
   NPC_QUITE
 };
+typedef struct {
+  int state;             // 状态标识（如运行、暂停、异常等）
+  uint32_t halt_pc;       // 停止时的程序计数器（PC）值
+  uint32_t halt_ret;     // 停止时的返回值或错误码
+} NPCState;
+
 extern NPCState npc_state;
 void sim_init();
 void step_and_dump_wave();
