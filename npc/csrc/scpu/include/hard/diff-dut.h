82812f7a5a54b9e4ff188a78f38baebcd41c424b
tracer-ysyx <tracer@ysyx.org>
2025-07-07 16:31:26 +0800

    > sim RTL ysyx_22040000 迟浩岐 Linux c 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:31:26 up 2 days,  6:47,  1 user,  load average: 0.50, 0.66, 0.54

--- npc/csrc/scpu/include/hard/diff-dut.h
+++ npc/csrc/scpu/include/hard/diff-dut.h
@@ -2,6 +2,6 @@
 #define __DIFF_DUT_H__
 
 #include <stdint.h>
-void init_difftest();
-bool difftest_step(uint32_t pc, uint32_t npc);
+void init_difftest(char *ref_so_file, long img_size);
+bool difftest_step();
 #endif
