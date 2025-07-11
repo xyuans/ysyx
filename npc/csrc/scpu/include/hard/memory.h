2c5dcfd8637959ecce1d0712c8cc58cda6d82e38
tracer-ysyx <tracer@ysyx.org>
2025-07-11 15:33:12 +0800

    > sim RTL ysyx_22040000 迟浩岐 Linux c 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:12 up 6 days,  5:49,  1 user,  load average: 0.24, 0.36, 0.50

--- npc/csrc/scpu/include/hard/memory.h
+++ npc/csrc/scpu/include/hard/memory.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define MEM_MAX 65536  // 2^16
+#define MEM_MAX 0x100000  // 2^20, 1MB
 uint64_t mem_init(char* filename);
 extern "C" uint32_t pmem_read(uint32_t addr);
 extern "C" void pmem_write(uint32_t waddr, uint32_t wdata, int len);
