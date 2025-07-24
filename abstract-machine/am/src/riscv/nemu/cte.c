9b2ad7d890bb06ca00fc72f50ab7ad1c67a5843c
tracer-ysyx <tracer@ysyx.org>
2025-07-24 19:20:30 +0800

    > compile NEMU ysyx_22040000 迟浩岐 Linux c 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:30 up 3 days,  9:50,  1 user,  load average: 0.56, 0.44, 0.49

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,22 +7,31 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    switch (c->mcause) {
+    #ifdef __riscv_e
+    int call_id = c->gpr[15];
+    #else
+    int call_id = c->gpr[17];
+    #endif 
+    switch (call_id) {  // c->gpr[17] is a7
+      case -1: 
+        ev.event = EVENT_YIELD; 
+        c->mepc += 4;
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
-
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
   return c;
 }
 
 extern void __am_asm_trap(void);
 
+// 传入处理回调函数handler, 设置自陷地址
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
-  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+  // 可能优化的原因是编译器认为 csrw mtvec 只是写入硬件寄存器，不影响程序内存状态
+  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));  // 将__am_asm_trap这个汇编定义的标签地址值放入寄存器中
 
   // register event handler
   user_handler = handler;
@@ -30,8 +39,14 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   return true;
 }
 
+// kstack是栈的范围, entry是内核线程的入口, arg则是内核线程的参数
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context *cxt = (Context *)((uint32_t *)kstack.end - NR_REGS - 4);  // 前向32个通用寄存器和四个特殊的寄存器
+  // 初始条件下gpr的上下文信息。
+  cxt->gpr[2] = (uint32_t)cxt;
+  cxt->gpr[10] = (uintptr_t)arg;
+  cxt->mepc = (uintptr_t)entry;
+  return cxt;
 }
 
 void yield() {
--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,7 +29,7 @@
 
 extern SymList symlist;  // ftrace.c
 
-CPU_state cpu = {};
+CPU_state cpu = {.mstatus = 0x1800};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
