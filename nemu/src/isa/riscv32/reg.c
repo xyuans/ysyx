ad480a248542b7b6e5db43ce143cc99a700f7bfc
tracer-ysyx <tracer@ysyx.org>
2025-07-29 11:19:05 +0800

    > compile NEMU ysyx_22040000 迟浩岐 Linux c 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:19:05 up 8 days,  1:48,  1 user,  load average: 0.81, 0.71, 0.57

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -21,13 +21,13 @@ const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
+  "mstatus", "mtvec", "mcause", "mepc"
 };
-
 void isa_reg_display() {
   printf(" %-10s%-#15x%-15d\n", "pc", cpu.pc, cpu.pc);
   for (int i=0; i<32; i++) {
-    printf(" %-10s%-#15x%-15d\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+    printf(" %-10s%-#15x%-15d\n", regs[i], *(word_t *)(cpu.gpr+i), cpu.gpr[i]);
   }
 }
 
