e3f31ff5fae9530c10a5e5c88c57bfc2a52e62c3
tracer-ysyx <tracer@ysyx.org>
2025-08-04 11:27:35 +0800

    > sim RTL ysyx_22040000 迟浩岐 Linux c 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:27:35 up 3 days, 22:27,  1 user,  load average: 0.50, 0.52, 0.44

--- npc/vsrc/scpu/Control.v
+++ npc/vsrc/scpu/Control.v
@@ -1,76 +1,91 @@
 /* verilator lint_off WIDTHTRUNC */
-module Control(
-  input [6:2] op,
-  input [14:12] funct3,
-  input funct7,  // inst[30]
-  output reg_write,
-  output [2:0] imm_src,
-  output alu_src,
-  output [3:0] alu_ctr,
-  output mem_write,
-  output [2:0] mem_op,
-  output [2:0] wd_src,
-  output [2:0] branch
-  
+module Control (
+    input [6:2] op,
+    input [14:12] funct3,
+    input [30:29] funct7,  // inst[30:29]
+    output reg_write,
+    output [2:0] imm_src,
+    output alu_src,
+    output [3:0] alu_ctr,
+    output mem_write,
+    output [2:0] mem_op,
+    output [2:0] wd_src,
+    output [2:0] branch,
+    output [1:0] csr_ctr
 );
-  reg [18:0] all_out;
-  assign {reg_write, imm_src, alu_src, alu_ctr, mem_write, mem_op, wd_src, branch} = all_out;
-  always @(*) begin 
-    casez({funct7, funct3, op})
-      // reg_write, imm_src, alu_src, alu_ctr, mem_write, wd_src, pc_src
+
+
+  reg [20:0] all_out;
+  assign {reg_write, imm_src, alu_src, alu_ctr, mem_write, mem_op, wd_src, branch, csr} = all_out;
+  always @(*) begin
+    casez ({
+      funct7, funct3, op
+    })
+      // reg_write, imm_src, alu_src, alu_ctr, mem_write, mem_op, wd_src, branch, csr
+      // reg_write:1-write 0-no write
       // imm_src: 000-i  001-s  010-b  011-u  100-j
       // alu_src: 0-rd2 1-imm
       // alu_ctr:
       //          alu_ctr[3]-sub/add or arith/logical
       //          alu_ctr[2:0]:000-sum 001-lshift 010-slt 011-sltu
       //                       100-xor 101-rshift 110-or  111-and or unsign/sign
-      // 
-      // mem_write:
+      //
+      // mem_write:1-write 2-no write
       // mem_op: 000-1byte,sign  001-2bs  010-4b  100-1bu  101-2bu
-      // wd_src: 000-alu_resualt  001-pc+4  010-imm  011-imm+pc 1xx:mem
-      // branch: 000-非跳转指令 001-jal, 010-jalr, 
+      // wd_src: 000-alu_resualt  001-pc+4  010-imm  011-imm+pc 100-mem 101-csr
+      // branch: 000-非跳转指令 001-jal, 010-jalr,
       //         100-beq, 101-bnq, 110-blt, 111-bge
-      // 
-      9'b?_???_01101: all_out = 19'b1_011_x_xxxx_0_xxx_010_000;  // lui,   u
-      9'b?_???_00101: all_out = 19'b1_011_x_xxxx_0_xxx_011_000;  // auipc, u
-      9'b?_???_11011: all_out = 19'b1_100_x_xxxx_0_xxx_001_001;  // jal,   j
-      9'b?_000_11001: all_out = 19'b1_000_1_0000_0_xxx_001_010;  // jalr,  i
-      9'b?_000_11000: all_out = 19'b0_010_0_1000_0_xxx_xxx_100;  // beq,   b
-      9'b?_001_11000: all_out = 19'b0_010_0_1xx0_0_xxx_xxx_101;  // bnq,   b
-      9'b?_100_11000: all_out = 19'b0_010_0_1xx0_0_xxx_xxx_110;  // blt,   b
-      9'b?_101_11000: all_out = 19'b0_010_0_1xx0_0_xxx_xxx_111;  // bge,   b
-      9'b?_110_11000: all_out = 19'b0_010_0_1xx1_0_xxx_xxx_110;  // bltu,  b
-      9'b?_111_11000: all_out = 19'b0_010_0_1xx1_0_xxx_xxx_111;  // bgeu,  b
-      9'b?_000_00000: all_out = 19'b1_000_1_0000_0_000_1xx_000;  // lb,    i
-      9'b?_001_00000: all_out = 19'b1_000_1_0000_0_001_1xx_000;  // lh,    i
-      9'b?_010_00000: all_out = 19'b1_000_1_0000_0_010_1xx_000;  // lw,    i
-      9'b?_100_00000: all_out = 19'b1_000_1_0000_0_100_1xx_000;  // lbu,   i
-      9'b?_101_00000: all_out = 19'b1_000_1_0000_0_101_1xx_000;  // lhu,   i
-      9'b?_000_01000: all_out = 19'b0_001_1_0000_1_000_xxx_000;  // sb,    s
-      9'b?_001_01000: all_out = 19'b0_001_1_0000_1_001_xxx_000;  // sh,    s
-      9'b?_010_01000: all_out = 19'b0_001_1_0000_1_010_xxx_000;  // sw,    s
-      9'b?_000_00100: all_out = 19'b1_000_1_0000_0_xxx_000_000;  // addi,  i
-      9'b?_010_00100: all_out = 19'b1_000_1_1010_0_xxx_000_000;  // slti,  i
-      9'b?_011_00100: all_out = 19'b1_000_1_1011_0_xxx_000_000;  // sltiu, i
-      9'b?_100_00100: all_out = 19'b1_000_1_x100_0_xxx_000_000;  // xori,  i
-      9'b?_110_00100: all_out = 19'b1_000_1_x110_0_xxx_000_000;  // ori,   i
-      9'b?_111_00100: all_out = 19'b1_000_1_x111_0_xxx_000_000;  // andi,  i
-      9'b0_001_00100: all_out = 19'b1_000_1_0001_0_xxx_000_000;  // slli,  i
-      9'b0_101_00100: all_out = 19'b1_000_1_0101_0_xxx_000_000;  // srli,  i
-      9'b1_101_00100: all_out = 19'b1_000_1_1101_0_xxx_000_000;  // srai,  i
-      9'b0_000_01100: all_out = 19'b1_xxx_0_0000_0_xxx_000_000;  // add,   r
-      9'b1_000_01100: all_out = 19'b1_xxx_0_1000_0_xxx_000_000;  // sub,   r
-      9'b0_001_01100: all_out = 19'b1_xxx_0_0001_0_xxx_000_000;  // sll,   r
-      9'b0_010_01100: all_out = 19'b1_xxx_0_1010_0_xxx_000_000;  // slt,   r
-      9'b0_011_01100: all_out = 19'b1_xxx_0_1011_0_xxx_000_000;  // sltu,  r
-      9'b0_100_01100: all_out = 19'b1_xxx_0_x100_0_xxx_000_000;  // xor,   r
-      9'b0_101_01100: all_out = 19'b1_xxx_0_0101_0_xxx_000_000;  // srl,   r
-      9'b1_101_01100: all_out = 19'b1_xxx_0_1101_0_xxx_000_000;  // sra,   r
-      9'b0_110_01100: all_out = 19'b1_xxx_0_x110_0_xxx_000_000;  // or,    r
-      9'b0_111_01100: all_out = 19'b1_xxx_0_x111_0_xxx_000_000;  // and,   r
-
-      default: all_out = 19'b0;
+      //csr:000-非csr相关  010-ecall  mret-011 101-mret100-mcsrrw  110-mcsrrs,
+      10'b?0_???_01101: all_out = 22'b1_011_x_xxxx_0_xxx_010_000_000;  // lui,   u
+      10'b?0_???_00101: all_out = 22'b1_011_x_xxxx_0_xxx_011_000_000;  // auipc, u
+      10'b?0_???_11011: all_out = 22'b1_100_x_xxxx_0_xxx_001_001_000;  // jal,   j
+      10'b?0_000_11001: all_out = 22'b1_000_1_0000_0_xxx_001_010_000;  // jalr,  i
+      10'b?0_000_11000: all_out = 22'b0_010_0_1000_0_xxx_xxx_100_000;  // beq,   b
+      10'b?0_001_11000: all_out = 22'b0_010_0_1xx0_0_xxx_xxx_101_000;  // bnq,   b
+      10'b?0_100_11000: all_out = 22'b0_010_0_1xx0_0_xxx_xxx_110_000;  // blt,   b
+      10'b?0_101_11000: all_out = 22'b0_010_0_1xx0_0_xxx_xxx_111_000;  // bge,   b
+      10'b?0_110_11000: all_out = 22'b0_010_0_1xx1_0_xxx_xxx_110_000;  // bltu,  b
+      10'b?0_111_11000: all_out = 22'b0_010_0_1xx1_0_xxx_xxx_111_000;  // bgeu,  b
+      10'b?0_000_00000: all_out = 22'b1_000_1_0000_0_000_100_000_000;  // lb,    i
+      10'b?0_001_00000: all_out = 22'b1_000_1_0000_0_001_100_000_000;  // lh,    i
+      10'b?0_010_00000: all_out = 22'b1_000_1_0000_0_010_100_000_000;  // lw,    i
+      10'b?0_100_00000: all_out = 22'b1_000_1_0000_0_100_100_000_000;  // lbu,   i
+      10'b?0_101_00000: all_out = 22'b1_000_1_0000_0_101_100_000_000;  // lhu,   i
+      10'b?0_000_01000: all_out = 22'b0_001_1_0000_1_000_xxx_000_000;  // sb,    s
+      10'b?0_001_01000: all_out = 22'b0_001_1_0000_1_001_xxx_000_000;  // sh,    s
+      10'b?0_010_01000: all_out = 22'b0_001_1_0000_1_010_xxx_000_000;  // sw,    s
+      10'b?0_000_00100: all_out = 22'b1_000_1_0000_0_xxx_000_000_000;  // addi,  i
+      10'b?0_010_00100: all_out = 22'b1_000_1_1010_0_xxx_000_000_000;  // slti,  i
+      10'b?0_011_00100: all_out = 22'b1_000_1_1011_0_xxx_000_000_000;  // sltiu, i
+      10'b?0_100_00100: all_out = 22'b1_000_1_x100_0_xxx_000_000_000;  // xori,  i
+      10'b?0_110_00100: all_out = 22'b1_000_1_x110_0_xxx_000_000_000;  // ori,   i
+      10'b?0_111_00100: all_out = 22'b1_000_1_x111_0_xxx_000_000_000;  // andi,  i
+      10'b00_001_00100: all_out = 22'b1_000_1_0001_0_xxx_000_000_000;  // slli,  i
+      10'b00_101_00100: all_out = 22'b1_000_1_0101_0_xxx_000_000_000;  // srli,  i
+      10'b10_101_00100: all_out = 22'b1_000_1_1101_0_xxx_000_000_000;  // srai,  i
+      10'b00_000_01100: all_out = 22'b1_xxx_0_0000_0_xxx_000_000_000;  // add,   r
+      10'b10_000_01100: all_out = 22'b1_xxx_0_1000_0_xxx_000_000_000;  // sub,   r
+      10'b00_001_01100: all_out = 22'b1_xxx_0_0001_0_xxx_000_000_000;  // sll,   r
+      10'b00_010_01100: all_out = 22'b1_xxx_0_1010_0_xxx_000_000_000;  // slt,   r
+      10'b00_011_01100: all_out = 22'b1_xxx_0_1011_0_xxx_000_000_000;  // sltu,  r
+      10'b00_100_01100: all_out = 22'b1_xxx_0_x100_0_xxx_000_000_000;  // xor,   r
+      10'b00_101_01100: all_out = 22'b1_xxx_0_0101_0_xxx_000_000_000;  // srl,   r
+      10'b10_101_01100: all_out = 22'b1_xxx_0_1101_0_xxx_000_000_000;  // sra,   r
+      10'b00_110_01100: all_out = 22'b1_xxx_0_x110_0_xxx_000_000_000;  // or,    r
+      10'b00_111_01100: all_out = 22'b1_xxx_0_x111_0_xxx_000_000_000;  // and,   r
+      10'b00_000_11100: all_out = 22'b0_xxx_x_xxxx_0_xxx_xxx_000_010;  // ecall, n
+      /*Zicsr*/
+      10'b??_001_11100: all_out = 22'b1_000_x_xxxx_0_xxx_101_000_101;  // csrrw,  i
+      10'b??_010_11100: all_out = 22'b1_000_x_xxxx_0_xxx_101_000_110;  // csrrs,  i
+      /*priveliged*/
+      10'b01_000_11100: all_out = 22'b0_xxx_x_xxxx_0_xxx_xxx_000_011;  // mret,  n
+      default: all_out = 22'b0;
     endcase
   end
 
 endmodule
+
+
+
+
+// reg_write, imm_src, alu_src, alu_ctr, mem_write, mem_op, wd_src, branch
--- npc/vsrc/scpu/top.v
+++ npc/vsrc/scpu/top.v
@@ -2,123 +2,144 @@
 import "DPI-C" function void ebreak();
 
 module top (
-  input clk,
-  input rst,
-  input [31:0] inst,
-  output reg [31:0] pc
+    input clk,
+    input rst,  // reset信号
+    input [31:0] inst,
+    output reg [31:0] pc
 );
-  
+
   //control unit用到的线
   wire reg_write, alu_src, mem_write;
-  wire [2:0] imm_src, mem_op, wd_src, branch;
+  wire [2:0] imm_src, mem_op, wd_src, branch, csr_ctr;
   wire [3:0] alu_ctr;
-  
+
   // regfile用到的线
   wire [31:0] rd1, rd2;
-  reg [31:0] wd;
+  reg  [31:0] wd;
 
   // ImmExt所用的线
   wire [31:0] imm;  // 作为输出的立即数
 
   // ALU用到的线
-  reg [31:0] alu_result;
+  reg  [31:0] alu_result;
   wire [31:0] alu_src2;
   wire less, zero;
 
   // pc相关
-  wire [31:0] pc_plus_4, imm_plus_pc;
-  reg [31:0] pc_result;
-  wire [1:0] pc_src;
+  wire [31:0] pc_plus_4, imm_plus_pc, csr_pc;
+  reg  [31:0] pc_result;
+  wire [ 1:0] pc_src;
 
   // DataMem
   wire [31:0] mem_rd;
-  
+
+  // Csr
+  wire [31:0] csr_rd;
+  // Control实例化
   Control control (
-    .op(inst[6:2]),
-    .funct3(inst[14:12]),
-    .funct7(inst[30]),
-    .reg_write(reg_write),
-    .imm_src(imm_src),
-    .alu_src(alu_src),
-    .alu_ctr(alu_ctr),
-    .mem_write(mem_write),
-    .mem_op(mem_op),
-    .wd_src(wd_src),
-    .branch(branch)
+      .op(inst[6:2]),
+      .funct3(inst[14:12]),
+      .funct7(inst[30]),
+      .reg_write(reg_write),
+      .imm_src(imm_src),
+      .alu_src(alu_src),
+      .alu_ctr(alu_ctr),
+      .mem_write(mem_write),
+      .mem_op(mem_op),
+      .wd_src(wd_src),
+      .branch(branch),
+      .csr_ctr(csr_ctr)
   );
 
-  RegFile #(.ADDR_WIDTH(5), .DATA_WIDTH(32)) rf (
-    .clk(clk),
-    .we(reg_write),
-    .ra1(inst[19:15]),
-    .ra2(inst[24:20]),
-    .rd1(rd1),
-    .rd2(rd2),
-    .wa(inst[11:7]),
-    .wd(wd)
+  RegFile #(
+      .ADDR_WIDTH(5),
+      .DATA_WIDTH(32)
+  ) rf (
+      .clk(clk),
+      .we (reg_write),
+      .ra1(inst[19:15]),
+      .ra2(inst[24:20]),
+      .rd1(rd1),
+      .rd2(rd2),
+      .wa (inst[11:7]),
+      .wd (wd)
   );
 
-  ImmExt immext(
-    .inst(inst[31:7]),
-    .imm_src(imm_src),
-    .imm(imm)
+  ImmExt immext (
+      .inst(inst[31:7]),
+      .imm_src(imm_src),
+      .imm(imm)
   );
-  
+
   // 对alu_src2的二选一
   Mux21 mux21 (
-    .a(rd2),  // rd2
-    .b(imm),  // imm 立即数
-    .s(alu_src),
-    .y(alu_src2)
+      .a(rd2),  // rd2
+      .b(imm),  // imm 立即数
+      .s(alu_src),
+      .y(alu_src2)
   );
 
   Alu alu (
-    .a(rd1),
-    .b(alu_src2),
-    .ctr(alu_ctr),
-    .y(alu_result),
-    .zero(zero),
-    .less(less)
+      .a(rd1),
+      .b(alu_src2),
+      .ctr(alu_ctr),
+      .y(alu_result),
+      .zero(zero),
+      .less(less)
   );
 
 
-  assign pc_plus_4 = 32'b100 + pc;
+  assign pc_plus_4   = 32'b100 + pc;
   assign imm_plus_pc = imm + pc;
-  
-  DataMem datamem(
-    .clk(clk),
-    .we(mem_write),
-    .ctr(mem_op),
-    .addr(alu_result),
-    .wd(rd2),
-    .rd(mem_rd)
+
+  DataMem datamem (
+      .clk (clk),
+      .we  (mem_write),
+      .ctr (mem_op),
+      .addr(alu_result),
+      .wd  (rd2),
+      .rd  (mem_rd)
+  );
+
+  // 实例化Csr模块
+  Csr csr (
+      .clk(clk),
+      .pc(pc),
+      .ctr(csr_ctr),
+      .addr(inst[31:20]),
+      .wd(rd1),
+      .rd(csr_rd),
+      .pc_out(csr_pc)
   );
 
   // 对wd_src的四选一
   always @(*) begin
-    casez(wd_src)
-      3'b000: wd = alu_result;
-      3'b001: wd = pc_plus_4;
-      3'b010: wd = imm;
-      3'b011: wd = imm_plus_pc;
-      3'b1??: wd = mem_rd;
+    casez (wd_src)
+      3'b000:  wd = alu_result;
+      3'b001:  wd = pc_plus_4;
+      3'b010:  wd = imm;
+      3'b011:  wd = imm_plus_pc;
+      3'b100:  wd = mem_rd;
+      3'b101:  wd = csr_rd;
       default: wd = 32'bx;
     endcase
   end
 
-  PcNext pcnext(
-    .branch(branch),
-    .zero(zero),
-    .less(less),
-    .pc_src(pc_src)
+  PcNext pcnext (
+      .csr_pc(csr_pc),
+      .branch(branch),
+      .zero  (zero),
+      .less  (less),
+      .pc_src(pc_src)
   );
 
   // 对pc_src的三选一
   always @(*) begin
-    case(pc_src)
-      2'b00: pc_result = pc_plus_4;
-      2'b01: pc_result = imm_plus_pc;   // jal
-      2'b10: pc_result = alu_result;    // jalr,此时alu_result=rd1 + imm
+    case (pc_src)
+      2'b00:   pc_result = pc_plus_4;
+      2'b01:   pc_result = imm_plus_pc;  // jal
+      2'b10:   pc_result = alu_result;  // jalr,此时alu_result=rd1 + imm
+      2'b11:   pc_result = csr_pc;
       default: pc_result = 32'bx;
     endcase
   end
@@ -127,7 +148,7 @@ module top (
   always @(posedge clk) begin
     if (rst) pc <= 32'h80000000;
     else begin
-      if (inst[6:0] == 7'b1110011) ebreak();
+      if ({inst[20], inst[6:0]} == 8'b1_1110011) ebreak();
       pc <= pc_result;
     end
   end
